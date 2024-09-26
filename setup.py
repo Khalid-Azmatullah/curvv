c5514c63806ab72ced0463a7b12d8adc525c2198
Khalid Azmatullah <khalid.studymaterial@gmail.com>
2024-09-26 22:34:36 +0530

    version-0.4.1

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as file:
+  longDescription = file.read()
+  file.close()
+with open("shD.md", "r") as file:
+  shortDescription = file.read()
+  file.close()
+
 setup(
   name="curvv",
-  version="0.2",
+  version="0.4.1",
   packages=find_packages(),
   install_requires=[
     #dependencies
-  ]
+  ],
+  description=shortDescription,
+  long_description=longDescription,
+  long_description_content_type="text/markdown"
 )
